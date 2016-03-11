619261a28292ffb8c0d3d876ff1d8829ee4a3b6b
BernardIO <satan5757@gmail.com>
2016-03-11 17:31:34 +0300

    Added generation of a new data base

--- labs/k618/DT.h
+++ labs/k618/DT.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <stdlib.h>
+#include <time.h>
 char names[20][20]={"Jacob","Michael","Joshua","Matthew","Ethan","Andrew","Daniel","William","Joseph","Christopher","Emily","Emma","Madison","Olivia",
 "Hannah","Abigail","Isabella","Ashley","Samantha","Elizabeth"};
 
@@ -8,7 +9,7 @@ char names[20][20]={"Jacob","Michael","Joshua","Matthew","Ethan","Andrew","Danie
 typedef struct Person{
 	char name[20];
 	int group;
-	int mark;
+	double mark;
 }Person;
 
 void test(){
@@ -19,15 +20,31 @@ void print_menu(){
 	printf("|   		     Menu   	               |\n");
 	printf("|----------------------------------------------|\n");
 	printf("|1.	Fill the base	 		       |\n");
-	printf("|2.     Add new element		  	       |\n");
-	printf("|3.	Remove the N-th element		       |\n");
-	printf("|4.	Remove all element with the same name  |\n");
-	printf("|5.	Option                                 |\n");
-	printf("|6.	Delete the base                        |\n");
-	printf("|7.     Quit                                   |\n");
+	printf("|2.	Generate a new base	 	       |\n");
+	printf("|3.     Add new element		  	       |\n");
+	printf("|4.	Remove the N-th element		       |\n");
+	printf("|5.	Remove all element with the same name  |\n");
+	printf("|6.	Option                                 |\n");
+	printf("|7.	Delete the base                        |\n");
+	printf("|8.     Quit                                   |\n");
 	printf("|----------------------------------------------|\n");
 }
-void generate_a_base(FILE *f){
+
+void generate_a_base(){
+	int amount;
+	FILE *f;
+	srand((unsigned) time(0) );
+	printf("Please enter amount of elements:\n");
+	scanf("%d\n",&amount);
+	f=fopen("Generated_base.dat","w+b");
+	for(int i=0;i<amount;i++){
+		int rand_names=rand()%20;
+		int group=rand()%8+1;
+		double mark=(rand()%100+1.0);
+		fprintf(f,"%s %d %f\n",names[rand_names],group,mark);
+	}
+	fclose(f);
+
 
 
 
