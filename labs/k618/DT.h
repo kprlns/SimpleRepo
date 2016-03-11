5e0e3cf901c24c2e1058a9476805839098ff98a0
BernardIO <satan5757@gmail.com>
2016-03-11 19:46:34 +0300

    Added generation of base

--- labs/k618/DT.h
+++ labs/k618/DT.h
@@ -7,7 +7,7 @@ char names[20][20]={"Jacob","Michael","Joshua","Matthew","Ethan","Andrew","Danie
 
 
 typedef struct Person{
-	char name[20];
+	char* name;
 	int group;
 	double mark;
 }Person;
@@ -37,30 +37,31 @@ void generate_a_base(){
 	printf("Please enter amount of elements:\n");
 	scanf("%d",&amount);
 	f=fopen("Generated_base","wb");
+	char c[10];
 	for(int i=0;i<amount;i++){
 		Person p;
-
 		int rand_names=rand()%20;
 		int group=rand()%8+1;
 		double mark=(rand()%100+1.0);
-		//fprintf(f,"%s %d %f\n",names[rand_names],group,mark);
-		fwrite(&names[rand_names],sizeof(char),1,f);
-		fwrite(&group,sizeof(int),1,f);
-		fwrite(&mark,sizeof(double),1,f);
+		p.name=names[rand_names];
+		p.group=group;
+		p.mark=mark;	
+		printf("%s %d %f\n",p.name,p.group,p.mark);
+		fwrite(&p, sizeof(p),1,f);
 	}
 	fclose(f);
 }
 void read_a_base(char* c[]){
-	FILE* f;
-	f=fopen(c,"rb");
-	char d[20];
-	while (true){
-		Person p;
-		fread(&p.name,1,10,f);
+	//FILE* f;
+	//f=fopen(&c,"rb");
+	//char d[20];
+	//while (1){
+	//	Person p;
+	//	fread(&p.name,1,10,f);
 		
 
 
-	}
+	//}
 
 
 }
