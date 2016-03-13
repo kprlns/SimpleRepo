0a791cded817a009aa4e36b0c525c258118d8d78
BernardIO <satan5757@gmail.com>
2016-03-13 18:53:51 +0300

    v1.1

--- labs/k618/DT.h
+++ labs/k618/DT.h
@@ -22,11 +22,11 @@ void print_menu(){
 	printf("|----------------------------------------------|\n");
 	printf("|1.	Fill the base	 		       |\n"); // REMOVE
 	printf("|2.	Generate a new base	 	       |\n"); // REMOVE
-	printf("|3.     Add new elements		       |\n"); //
-	printf("|4.     Print the base		  	       |\n"); //
-	printf("|5.	Remove the N-th element		       |\n");
+	printf("|3.     Add new elements		       |\n"); //DONE
+	printf("|4.     Print the base		  	       |\n"); //DONE
+	printf("|5.	Remove the N-th element		       |\n"); //DONE ????TESTING????
 	printf("|6.	Remove all element with the same name  |\n");
-	printf("|7.	Option                                 |\n");
+	printf("|7.	Option 18                              |\n");
 	printf("|8.	Delete the base                        |\n");
 	printf("|9.     Quit                                   |\n");
 	printf("|----------------------------------------------|\n");
@@ -41,16 +41,62 @@ void add_new_elements(char* path){
 	//printf("%d",sizeof(p.marks));
 	for(int i=0;i<a;i++){
 		scanf("%s%s%s%d",&p.name,&p.initial,&p.gen,&p.group);
-		for(int j=0;j<a;j++){
+		for(int j=0;j<4;j++){
 			scanf("%d",&p.marks[j]);
 		}
 		fwrite(&p,sizeof(Person),1,f);
 	}
-	Person q;
-	rewind(f);
-	fread(&q,sizeof(Person),1,f);
-	printf("%s %s %s",q.name,q.initial,q.gen);
+	//Person q;
+	//rewind(f);
+	//fread(&q,sizeof(Person),1,f);
+	//printf("%s %s %s",q.name,q.initial,q.gen);
 	fclose(f);
+}
+void print_a_base(char* path){
+	FILE* f;
+	f=fopen(path,"rb");
+	Person q;
+	while(fread(&q,sizeof(Person),1,f)>0){
+		printf("%s %s %s %d ",q.name,q.initial,q.gen,q.group);
+		for(int j=0;j<4;j++){
+				printf("%d ",q.marks[j]);
+		}
+		printf("\n");
+	}
+	fclose(f);
+}
+void remove_nth_element(char* path,int n){
+	FILE* f;
+	FILE* temp;
+	f=fopen(path,"rb");
+	temp=fopen("temp.dat","w+b");
+	int i=0;
+	Person q;
+	while(fread(&q,sizeof(Person),1,f)>0){
+		if(i==n){
+			i++;
+			continue;
+		}
+		else{
+			//fread(&q,sizeof(Person),1,f);
+			fwrite(&q,sizeof(Person),1,temp);
+		}			
+		i++;
+	}
+
+	fclose(temp);
+	temp=fopen("temp.dat","rb");
+	fclose(f);
+	f=fopen(path,"wb");
+	
+	while(fread(&q,sizeof(Person),1,temp)>0){
+			fread(&q,sizeof(Person),1,temp);
+			fwrite(&q,sizeof(Person),1,f);	
+	}
+	fclose(temp);
+	fclose(f);
+	remove("temp.dat");
+	
 }
 
 
--- labs/k618/k618.c
+++ labs/k618/k618.c
@@ -6,10 +6,13 @@ int main(int args,char* arg[]){
 	//for(int i=0;i<17;i++)
 	//	printf("%s\n",inits[i]);
 	//generate_a_base();
-	//write_a_base("Generated_base");
-	add_new_elements("Generated_base");
- 	//write_a_base("Generated_base");
-	//write_a_base("Generated_base");
+	print_a_base("Generated_base");
+	printf("------------------\n");
+	//add_new_elements("Generated_base");
+	remove_nth_element("Generated_base",0);
+	print_a_base("Generated_base");
+ 	//print_a_base("Generated_base");
+	//print_a_base("Generated_base");
 	return 0;
 }
 
