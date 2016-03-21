e47428356252d59a9580c1b5b20d9c80f38a4134
BernardIO <satan5757@gmail.com>
2016-03-21 19:51:17 +0300

    v1.6

--- labs/k618/DT.h
+++ labs/k618/DT.h
@@ -16,26 +16,26 @@ int cmp(char* a,char* b){
 	for(int i=0;i<10;i++){
 		if(a[i]=='\0' && b[i]==a[i]) return 1;
 		if(a[i]!=b[i]) return 0;
-	}	
+	}
 	return 1;
 }
 Person new_person(char* name,char* init,char* genn){
 		Person p;
 		//srand((unsigned) time(0) );
 		for(int i=0;i<10;i++){
-			p.name[i]=name[i];			
+			p.name[i]=name[i];
 		}
 		for(int i=0;i<5;i++){
 			p.initial[i]=init[i];
 			if(i<4){
 				p.marks[i]=rand()%4+2;
-			}			
+			}
 		}
 		for(int i=0;i<2;i++){
-			p.gen[i]=genn[i];			
+			p.gen[i]=genn[i];
 		}
 		p.group=rand()%8+1;
-	
+
 		return p;
 }
 
@@ -60,115 +60,128 @@ void print_menu(){
 void create_a_base(char* path){
 	FILE* f;
 	f=fopen(path,"wb");
-	int a;
-	printf("Please enter a number of elemts:\n");
-	scanf("%d",&a);
-	srand((unsigned) time(0) );
-	for(int i=0;i<a;i++){
-		int rand_names=rand()%20;
-		int rand_init=rand()%17;
-		char gen[2];
-		if(rand_names<10) 
-			gen[0]='M';		
-		else	
-			gen[0]='F';
-		gen[1]='\0';
-		Person p=new_person(names[rand_names],inits[rand_init],&gen);
-		fwrite(&p,sizeof(p),1,f);  	
+	if(f){
+		int a;
+		printf("Please enter a number of elemts:\n");
+		scanf("%d",&a);
+		srand((unsigned) time(0) );
+		for(int i=0;i<a;i++){
+			int rand_names=rand()%20;
+			int rand_init=rand()%17;
+			char gen[2];
+			if(rand_names<10)
+				gen[0]='M';
+			else
+				gen[0]='F';
+			gen[1]='\0';
+			Person p=new_person(names[rand_names],inits[rand_init],&gen);
+			fwrite(&p,sizeof(p),1,f);
+		}
+		fclose(f);
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-	fclose(f);
 }
 void add_random_elements(char* path){
 	FILE* f;
 	f=fopen(path,"a+b");
-	int a;
-	printf("Please enter a number of elements:\n");
-	scanf("%d",&a);
-	srand((unsigned) time(0) );
-	for(int i=0;i<a;i++){
-		int rand_names=rand()%20;
-		int rand_init=rand()%17;
-		char gen[2];
-		if(rand_names<10) 
-			gen[0]='M';		
-		else	
-			gen[0]='F';
-		gen[1]='\0';
-		Person p=new_person(names[rand_names],inits[rand_init],&gen);
-		fwrite(&p,sizeof(p),1,f);  	
-	}
+	if(f){
+		int a;
+		printf("Please enter a number of elements:\n");
+		scanf("%d",&a);
+		srand((unsigned) time(0) );
+		for(int i=0;i<a;i++){
+			int rand_names=rand()%20;
+			int rand_init=rand()%17;
+			char gen[2];
+			if(rand_names<10)
+				gen[0]='M';
+			else
+				gen[0]='F';
+			gen[1]='\0';
+			Person p=new_person(names[rand_names],inits[rand_init],&gen);
+			fwrite(&p,sizeof(p),1,f);
+		}
 	fclose(f);
-
-
+	}else{
+		printf("I can not find or open the file.\n");
+	}
 }
-
 void add_new_elements(char* path){
 	FILE* f;
 	f=fopen(path,"a+b");
-	Person p;
-	int a;
-	printf("Please enter a number of elements:\n");
-	scanf("%d",&a);
-	//printf("%d",sizeof(p.marks));
-	for(int i=0;i<a;i++){
-		scanf("%s%s%s%d",&p.name,&p.initial,&p.gen,&p.group);
-		for(int j=0;j<4;j++){
-			scanf("%d",&p.marks[j]);
-			if(p.marks[j]<2 || p.marks[j]>5) {
-				p.marks[j]=2;
+	if(f){
+		Person p;
+		int a;
+		printf("Please enter a number of elements:\n");
+		scanf("%d",&a);
+		for(int i=0;i<a;i++){
+			scanf("%s%s%s%d",&p.name,&p.initial,&p.gen,&p.group);
+			for(int j=0;j<4;j++){
+				scanf("%d",&p.marks[j]);
+				if(p.marks[j]<2 || p.marks[j]>5) {
+					p.marks[j]=2;
+				}
 			}
+			fwrite(&p,sizeof(Person),1,f);
 		}
-		fwrite(&p,sizeof(Person),1,f);
+		fclose(f);
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-	//Person q;
-	//rewind(f);
-	//fread(&q,sizeof(Person),1,f);
-	//printf("%s %s %s",q.name,q.initial,q.gen);
-	fclose(f);
 }
 void print_a_base(char* path){
 	FILE* f;
 	f=fopen(path,"rb");
-	Person q;
-	int i=1;
-	while(fread(&q,sizeof(Person),1,f)>0){
-		printf("№%d ",i);
-		printf("%s %s %s\nGroup: %d\nMarks: ",q.name,q.initial,q.gen,q.group);
-		for(int j=0;j<4;j++){
-				printf("%d ",q.marks[j]);
+	if(f){
+		Person q;
+		int i=1;
+		while(fread(&q,sizeof(Person),1,f)>0){
+			printf("№%d ",i);
+			printf("%s %s %s\nGroup: %d\nMarks: ",q.name,q.initial,q.gen,q.group);
+			for(int j=0;j<4;j++){
+					printf("%d ",q.marks[j]);
+			}
+			printf("\n");
+			i++;
 		}
-		printf("\n");
-		i++;
+		fclose(f);
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-	fclose(f);
 }
 void remove_nth_element(char* path,int n){
 	FILE* f;
 	FILE* temp;
 	f=fopen(path,"rb");
 	temp=fopen("temp.dat","w+b");
-	int i=0;
-	Person q;
-	while(fread(&q,sizeof(Person),1,f)>0){
-		if(i!=n){
-			//fread(&q,sizeof(Person),1,f);
-			fwrite(&q,sizeof(Person),1,temp);
-			i++;
-		}	else		
-	        	i++;
+	if(f&&temp){
+		int i=0;
+		Person q;
+		while(fread(&q,sizeof(Person),1,f)>0){
+			if(i!=n){
+				//fread(&q,sizeof(Person),1,f);
+				fwrite(&q,sizeof(Person),1,temp);
+				i++;
+			}	else
+			    	i++;
+		}
+		rewind(temp);
+		fclose(f);
+		f=fopen(path,"wb");
+
+		while(fread(&q,sizeof(Person),1,temp)>0){
+				fwrite(&q,sizeof(Person),1,f);
+		}
+		fclose(temp);
+		fclose(f);
+		//print_a_base("temp.dat");
+		remove("temp.dat");
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-    rewind(temp);
-	fclose(f);
-	f=fopen(path,"wb");
-	
-	while(fread(&q,sizeof(Person),1,temp)>0){
-			fwrite(&q,sizeof(Person),1,f);	
-	}
-	fclose(temp);
-	fclose(f);
-	//print_a_base("temp.dat");
-	remove("temp.dat");
-	
+
+
 }
 
 void delete_with_same_name(char* path,char* name){
@@ -179,60 +192,76 @@ void delete_with_same_name(char* path,char* name){
 	//printf("3\n");
 	temp=fopen("temp2.dat","w+b");
 	//printf("3\n");
-	Person q;
-	while(fread(&q,sizeof(Person),1,f)){
-		//printf("%d\n",cmp(&q.name,name));
-		if(!cmp(&q.name,name))
-			fwrite(&q,sizeof(Person),1,temp);
+	if(f&&temp){
+		Person q;
+		while(fread(&q,sizeof(Person),1,f)){
+			//printf("%d\n",cmp(&q.name,name));
+			if(!cmp(&q.name,name))
+				fwrite(&q,sizeof(Person),1,temp);
+		}
+		rewind(temp);
+		fclose(f);
+		f=fopen(path,"wb");
+
+		while(fread(&q,sizeof(Person),1,temp)>0){
+				fwrite(&q,sizeof(Person),1,f);
+		}
+		fclose(temp);
+		fclose(f);
+		//print_a_base("temp.dat");
+		remove("temp2.dat");
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-    rewind(temp);
-	fclose(f);
-	f=fopen(path,"wb");
-	
-	while(fread(&q,sizeof(Person),1,temp)>0){
-			fwrite(&q,sizeof(Person),1,f);	
-	}
-	fclose(temp);
-	fclose(f);
-	//print_a_base("temp.dat");
-	remove("temp2.dat");
 
 
 }
 void solution_18(char* path){
 	FILE* f;
 	f=fopen(path,"rb");
-	int diff[9];
-	for(int i=0;i<9;i++) diff[i]=0;
-	Person q;
-	while(fread(&q,sizeof(Person),1,f)){
-		int mx=0,mn=6;
-		for(int i=0;i<4;i++){
-			if(q.marks[i]<mn) mn=q.marks[i];
-			if(q.marks[i]>mx) mx=q.marks[i];
-		}	
-		diff[q.group]=abs(mx-mn);
-	}
-	int mx=-1,group=-1;
-	for(int i=1;i<9;i++){
-		if(diff[i]>mx) {
-			mx=diff[i];
-			group=i;
+	if(f){
+		double mx[9],mn[9];
+		for(int i=0;i<9;i++){
+			mx[i]=-1;
+			mn[i]=6;
 		}
+		Person q;
+		while(fread(&q,sizeof(Person),1,f)){
+				int sum=0;
+				for (int i = 0; i < 4; i++) {
+					sum+=q.marks[i];
+				}
+				double mean=sum/4.0;
+				if(mean>mx[q.group]){
+						mx[q.group]=mean;
+				}
+				if(mean<mn[q.group]){
+						mn[q.group]=mean;
+				}
+		}
+		int group=-1;
+		double difference=-2;
+		for (int i = 1; i < 9; i++) {
+			 if(mx[i]-mn[i] > difference ){
+				 	difference=mx[i]-mn[i];
+					group=i;
+			 }
+		}
+		printf("Group with maximum difference is %d-th with difference: %.2f \n",group,difference);
+		fclose(f);
+	}else{
+		printf("I can not find or open the file.\n");
 	}
-	printf("Group with maximum difference is %d-th \n",group);
-	fclose(f);
 }
-
-
 
 void erase_the_base(char* path){
 	FILE* f;
 	f=fopen(path,"wb");
-	fclose(f);
+	if(f)
+		fclose(f);
+	else
+		printf("I can not find or open the file.\n");
 }
-
 void quit(){
 	exit(0);
 }
-
--- labs/k618/k618.c
+++ labs/k618/k618.c
@@ -2,23 +2,23 @@
 #include "DT.h"
 
 int main(int args,char* arg[]){
-	
+
 	//char nam[10]="mike";
 	create_a_base("Generated");
 	//for(int i=0;i<17;i++)
 	//	printf("%s\n",inits[i]);
 	//generate_a_base();
 	print_a_base("Generated");
-	add_random_elements("Generated");
+	//add_random_elements("Generated");
+	solution_18("Generated");
 	//printf("------------------\n");
 	//add_new_elements("Generated_base");
 	//remove_nth_element("Generated_base",0);
 	//delete_with_same_name("Generated_base",&nam);
 	//solution_18("Generated_base");
-	print_a_base("Generated");
+	//print_a_base("Generated");
  	//print_a_base("Generated_base");
 	//print_a_base("Generated_base");
-		
+
 	return 0;
 }
-
